d616bf9974903af49164e264f2195ddf2fc84895
Ramya Erramilli <e.ramya.103@gmail.com>
2020-05-03 15:10:24 +0530

    Initial Commit

---
+++ TipsySwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TipsySwiftUI
+//
+//  Created by Ramya Seshagiri on 03/05/20.
+//  Copyright © 2020 Ramya Seshagiri. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
